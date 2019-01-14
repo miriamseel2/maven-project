6ca6405f23e2b6c11d1a245de5e9643eb00164cb
Seel <mseel@mt-ag.com>
2019-01-14 14:07:12 +0100

    Changed output format to json

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -2,6 +2,7 @@ package com.mulesoft.training;
 
 import static org.junit.Assert.*;
 
+
 import org.junit.Test;
 import org.mule.tck.junit4.FunctionalTestCase;
 
@@ -9,7 +10,7 @@ public class HelloMavenTest extends FunctionalTestCase {
 
     @Test
     public void mavenFlowReturnsHelloMaven() throws Exception {
-        runFlowAndExpect("mavenFlow", "[{DESTINATION=SFO, PRICE=555, ORIGIN=YYZ, ID=0}, {DESTINATION=LAX, PRICE=450, ORIGIN=YYZ, ID=1}, {DESTINATION=SEA, PRICE=777, ORIGIN=SQL, ID=2}, {DESTINATION=SFO, PRICE=999, ORIGIN=SQL, ID=3}]");
+        runFlowAndExpect("mavenFlow", "Hello Maven");
     }
     
     @Override
